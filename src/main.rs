f5125b5f6fc8747f531320feb5a9bd057e53f6ce
Jia Ye <planet0104@gmail.com>
2018-08-08 11:58:23 +0800

    parse ok.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_json;
 
 use serde_json::{Value, Error};
 use serde_json::map::Map;
+use sdl2::pixels::Color;
  
 mod cnn;
 mod data;
@@ -36,7 +37,7 @@ fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     
-    let window = video_subsystem.window("边缘检测", 500, 500)
+    let window = video_subsystem.window("nn", 520, 520)
       .position_centered()
       .build()
       .unwrap();
@@ -51,9 +52,16 @@ fn main() {
     // }
     controller.render(&mut canvas);
 
-    parse_html();
-
-    return;
+    let (key, draw) = parse_html(&String::from_utf8_lossy(fan_html)).unwrap();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    //canvas.clear();
+    let scale = 0.5;
+    canvas.set_scale(scale, scale);
+    for i in 0..draw.len(){
+        println!("第{}笔:{:?}", i, draw[i]);
+        canvas.draw_lines(draw[i].as_slice()).unwrap();
+    }
+    canvas.present();
 
     'mainloop: loop {
             for event in sdl_context.event_pump().unwrap().poll_iter() {
@@ -81,14 +89,12 @@ fn main() {
     }
 }
 
-fn parse_html(){
+fn parse_html<'a>(html: &str)->Option<(String, Vec<Vec<Point>>)>{
     /*
 
 	hzbh.main('繁', 繁:[17,'0:(162,18) (186,36) (138,96) (96,144) (30,204)#1:(138,96) (420,96) (378,78) (336,96)#2:(144,138) (108,336) (84,354) (108,336) (444,336) (402,324) (366,336)#3:(138,162) (360,162) (390,138) (360,162) (330,360)#4:(192,168) (246,204) (264,228) (270,246)#5:(24,252) (462,252) (420,234) (384,252)#6:(192,252) (246,276) (264,300) (270,324)#7:(528,18) (552,30) (510,96) (474,144) (444,186)#8:(498,114) (726,114) (684,96) (648,114)#9:(654,114) (636,162) (612,216) (582,264) (546,306) (492,354) (438,390)#10:(486,132) (522,210) (552,258) (588,300) (630,336) (660,360) (714,390)#11:(312,360) (348,366) (198,456) (162,468) (198,456) (402,444)#12:(468,384) (498,396) (348,474) (150,564) (114,576) (150,564) (576,540)#13:(480,474) (552,516) (594,552) (618,588)#14:(390,552) (390,708) (378,732) (348,762) (270,702)#15:(234,594) (276,612) (192,672) (120,714) (54,744)#16:(480,606) (540,636) (618,684) (690,738)']});hzbh.flash('繁','fj/fan7');
     */
-    let doc = String::from_utf8_lossy(ma_html);
-   // println!("{:#?}", doc);
-   let s = doc.split("hzbh.main(");
+   let s = html.split("hzbh.main(");
    if let Some(s) = s.skip(1).next(){
        let mut s = s.split(");");
        if let Some(s) = s.next(){
@@ -103,10 +109,43 @@ fn parse_html(){
                 let mut string = String::from(value.next().unwrap());
                 println!("汉字={}", key);
                 println!("笔画数={}", count);
-                for i in 0..count.parse().unwrap(){
-                    string = string.replace(&format!("{}:", i), "");
+                let mut result = vec![];
+                string.replace_range(0..2, "");
+                for i in 1..count.parse().unwrap(){
+                    string = string.replace(&format!("#{}:", i), "#");
                 }
-                println!("{}", string);
+                let arr = string.split("#");
+                
+                let mut i = 0;
+                for b in arr{
+                    i+=1;
+                    let mut points:Vec<Point> = b.split(" ").map(|p|{
+                        let xy:Vec<&str> = p.trim_right_matches(")")
+                        .trim_left_matches("(").split(",").collect();
+                        Point::new(xy[0].parse().unwrap(), xy[1].parse().unwrap())
+                    }).collect();
+                    //横线末尾的沟删掉
+                    if points.len()>=4 &&
+                        points[points.len()-1].y == points[points.len()-3].y{
+                        points.pop();
+                        points.pop();
+                    }
+                    //折线中间的突起去掉
+                    let mut new_points:Vec<Point> = vec![];
+                    for point in points{
+                        let len = new_points.len();
+                        if len>=2 && new_points[len-2].x == point.x &&
+                                        new_points[len-2].y == point.y{
+                            new_points.pop();
+                        }else{
+                            new_points.push(point);
+                        }
+                    }
+
+                    result.push(new_points);
+                }
+
+                return Some((key.to_string(), result));
                 
             }else{
                 println!("没有找到花括号");
@@ -118,4 +157,5 @@ fn parse_html(){
        println!("没有找到hzbh.main(");
    }
    
+   None
 }
