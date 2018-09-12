8bb4be60010f03d447214555b59d57fe8ad136f9
Jia Ye <planet0104@gmail.com>
2018-09-12 21:29:09 +0800

    compress

--- dollar/html/split_gif/src/main.rs
+++ dollar/html/split_gif/src/main.rs
@@ -15,6 +15,15 @@ mod pdollarplus;
 //矢量化 http://potrace.sourceforge.net/
 //矢量化 http://autotrace.sourceforge.net/
 
+/*
+设置通过难度级别
+线条压缩:
+1、多个连续点的x相等，或者多个连续点的y相等，删除中间的点
+支持多字横屏书写
+每个笔画多边形，中心线细化
+支持多个字一起写
+*/
+
 use bincode::serialize;
 use std::fs::File;
 use std::io::prelude::*;
@@ -23,7 +32,7 @@ use gif_dispose::Screen;
 use image::{ImageBuffer, RgbImage, Rgb};
 
 fn main() {
-    let file = File::open("ren.gif").unwrap();
+    let file = File::open("tao.gif").unwrap();
     let mut decoder = Decoder::new(file);
 
     // Important:
@@ -110,23 +119,429 @@ fn main() {
     //过滤无效的笔画
     strokes.retain(|stroke| !(stroke.len()==1 && stroke[0].len()<30));
 
-    //减少点数
-    // let strokes = {
-    //     let mut new_strokes = vec![];
-    //     for contours in strokes{
-    //         let mut new_contours = vec![];
-    //         for points in contours{
-    //             //减少点数
-    //             use pdollarplus::Point;
-    //             let points = pdollarplus::resample(points.iter().map(|point|{
-    //                 Point::new(point.x as f64, point.y as f64, 1)
-    //             }).collect(), 50);
-    //             new_contours.push(points);
-    //         }
-    //         new_strokes.push(new_contours);
-    //     }
-    //     new_strokes
-    // };
+    //压缩数据
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor<block_len && block[cursor].x == block[cursor-1].x{//检查竖线
+                        cursor += 1;
+                        count += 1;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        if count == 1{
+                            new_points.push(block[cursor-1].clone());    
+                        }else{
+                            new_points.push(block[cursor].clone());
+                            cursor += 1;
+                        }
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    //横线过滤
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor<block_len && block[cursor].y == block[cursor-1].y{//检查横线
+                        cursor += 1;
+                        count += 1;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        if count == 1{
+                            new_points.push(block[cursor-1].clone());    
+                        }else{
+                            new_points.push(block[cursor].clone());
+                            cursor += 1;
+                        }
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    //斜线过滤
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor<block_len && block[cursor].y != block[cursor-1].y && block[cursor].x != block[cursor-1].x{//检查横线
+                        cursor += 1;
+                        count += 1;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        if count == 1{
+                            new_points.push(block[cursor-1].clone());    
+                        }else{
+                            new_points.push(block[cursor].clone());
+                            cursor += 1;
+                        }
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    //--------- 14.9K ------
+
+    //两个点的斜线过滤
+    /*
+          --
+            --
+              --
+    */
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+2<block_len
+                        && block[cursor+1].x == block[cursor].x+1
+                        && block[cursor+1].y == block[cursor].y+1
+                        && block[cursor+2].y == block[cursor].y+1
+                    {//检查横线
+                        cursor += 2;
+                        count += 2;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    //--------- 14.5K --------------
+
+    /*
+            --
+          --
+        --
+    */
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+2<block_len
+                        && block[cursor+1].x == block[cursor].x-1
+                        && block[cursor+1].y == block[cursor].y+1
+                        && block[cursor+2].y == block[cursor].y+1
+                    {//检查横线
+                        cursor += 2;
+                        count += 2;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+2<block_len
+                        && block[cursor+1].x == block[cursor].x-1
+                        && block[cursor+1].y == block[cursor].y-1
+                        && block[cursor+2].y == block[cursor].y-1
+                    {//检查横线
+                        cursor += 2;
+                        count += 2;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+2<block_len
+                        && block[cursor+1].x == block[cursor].x+1
+                        && block[cursor+1].y == block[cursor].y-1
+                        && block[cursor+2].y == block[cursor].y-1
+                    {//检查横线
+                        cursor += 2;
+                        count += 2;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    //--------- 13.8K --------------
+
+    //三个点的斜线过滤
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+3<block_len
+                        && block[cursor+1].x == block[cursor].x+1
+                        && block[cursor+1].y == block[cursor].y+1
+                        && block[cursor+2].y == block[cursor].y+1
+                        && block[cursor+3].y == block[cursor].y+1
+                    {//检查横线
+                        cursor += 3;
+                        count += 3;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+3<block_len
+                        && block[cursor+1].x == block[cursor].x-1
+                        && block[cursor+1].y == block[cursor].y+1
+                        && block[cursor+2].y == block[cursor].y+1
+                        && block[cursor+3].y == block[cursor].y+1
+                    {//检查横线
+                        cursor += 3;
+                        count += 3;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+3<block_len
+                        && block[cursor+1].x == block[cursor].x-1
+                        && block[cursor+1].y == block[cursor].y-1
+                        && block[cursor+2].y == block[cursor].y-1
+                        && block[cursor+3].y == block[cursor].y-1
+                    {//检查横线
+                        cursor += 3;
+                        count += 3;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    let strokes = {
+        let mut new_strokes = vec![];
+        for stroke in strokes{//每一笔
+            let mut new_stroke = vec![];
+            for block in stroke{//每一块
+                let mut new_points = vec![];
+                //每个点
+                new_points.push(block[0].clone());
+                let mut cursor = 1;
+                let block_len = block.len();
+                while cursor<block_len{
+                    let mut count = 0;
+                    while cursor+3<block_len
+                        && block[cursor+1].x == block[cursor].x+1
+                        && block[cursor+1].y == block[cursor].y-1
+                        && block[cursor+2].y == block[cursor].y-1
+                        && block[cursor+3].y == block[cursor].y-1
+                    {//检查横线
+                        cursor += 3;
+                        count += 3;
+                    }
+                    if count>1{
+                        new_points.push(block[cursor-count].clone());
+                        new_points.push(block[cursor-1].clone());
+                    }else{
+                        new_points.push(block[cursor].clone());
+                        cursor += 1;
+                    }
+                }
+                new_stroke.push(new_points);
+            }
+            new_strokes.push(new_stroke);
+        }
+
+        new_strokes
+    };
+
+    //--------- 11.6K --------------
 
     println!("笔画数量:{}", strokes.len());
 
@@ -145,36 +560,36 @@ fn main() {
     //绘图
     let mut image = RgbImage::new(300, 300);
     //画1笔
-    imageproc::drawing::draw_filled_rect_mut(&mut image, 
-        imageproc::rect::Rect::at(0,0).of_size(300,300), Rgb([255, 255, 255]));
-    let points = strokes[0][0];
-    println!("{:?}", points);
+    // imageproc::drawing::draw_filled_rect_mut(&mut image, 
+    //     imageproc::rect::Rect::at(0,0).of_size(300,300), Rgb([255, 255, 255]));
+    // let points = strokes[0][0];
+    // println!("{:?}", points);
 
-    let data:Vec<(i16,i16)> = points.iter().map(|point|{
-        (point.x as i16, point.y as i16)
-    }).collect();
-    let encoded: Vec<u8> = serialize(&data).unwrap();
-    let mut file = File::create("points.data").unwrap();
-    file.write_all(&encoded).unwrap();
+    // let data:Vec<(i16,i16)> = points.iter().map(|point|{
+    //     (point.x as i16, point.y as i16)
+    // }).collect();
+    // let encoded: Vec<u8> = serialize(&data).unwrap();
+    // let mut file = File::create("points.data").unwrap();
+    // file.write_all(&encoded).unwrap();
 
-    for i in 1..points.len(){
-        imageproc::drawing::draw_line_segment_mut(&mut image,
-        (points[i-1].x as f32, points[i-1].y as f32),
-        (points[i].x as f32, points[i].y as f32),
-        Rgb([0, 0, 0]));
-    }
-
-    // for contours in strokes{
-    //     let mut color = Rgb([rand::random(), rand::random(), rand::random()]);
-    //     for points in contours{
-    //         for i in 1..points.len(){
-    //             imageproc::drawing::draw_line_segment_mut(&mut image,
-    //             (points[i-1].x as f32, points[i-1].y as f32),
-    //             (points[i].x as f32, points[i].y as f32),
-    //             color);
-    //         }
-    //     }
+    // for i in 1..points.len(){
+    //     imageproc::drawing::draw_line_segment_mut(&mut image,
+    //     (points[i-1].x as f32, points[i-1].y as f32),
+    //     (points[i].x as f32, points[i].y as f32),
+    //     Rgb([0, 0, 0]));
     // }
+
+    for contours in strokes{
+        let mut color = Rgb([rand::random(), rand::random(), rand::random()]);
+        for points in contours{
+            for i in 1..points.len(){
+                imageproc::drawing::draw_line_segment_mut(&mut image,
+                (points[i-1].x as f32, points[i-1].y as f32),
+                (points[i].x as f32, points[i].y as f32),
+                color);
+            }
+        }
+    }
     image.save("out.bmp").unwrap();
 
     // image::save_buffer("out.bmp", &buffer, 300, 300, image::RGB(8)).unwrap();
